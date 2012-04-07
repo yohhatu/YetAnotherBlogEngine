800ea9d5dd90bbdaa6fc1c8a533859e844f8a3c3
yohhatu <yohhatu@gmail.com>
2012-04-07 14:50:37 +0900

    Userモデルとテストコードを追加

--- test/BasicTest.java
+++ test/BasicTest.java
@@ -1,5 +1,8 @@
 import org.junit.*;
 import java.util.*;
+
+import javax.jws.soap.SOAPBinding.Use;
+
 import play.test.*;
 import models.*;
 
@@ -8,6 +11,26 @@ public class BasicTest extends UnitTest {
     @Test
     public void aVeryImportantThingToTest() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void createAndRetrieveUser(){
+    	new User("bob@example.com", "secret", "Bob").save();
+
+    	User bob = User.find("byEmail", "bob@example.com").first();
+
+    	assertNotNull(bob);
+    	assertEquals("Bob", bob.fullname);
+    }
+
+    @Test
+    public void tryConnectAsUser(){
+    	new User("bob@example.com", "secret", "Bob").save();
+
+    	assertNotNull(User.connect("bob@example.com", "secret"));
+    	assertNull(User.connect("bob@example.com", "badpassword"));
+    	assertNull(User.connect("tom@example.com", "secret"));
+
     }
 
 }
