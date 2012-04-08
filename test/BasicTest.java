5447831a685aaa7a44c67ddebfb81698422f1478
yohhatu <yohhatu@gmail.com>
2012-04-08 14:51:52 +0900

    Commentモデルとテストコードを追加

--- test/BasicTest.java
+++ test/BasicTest.java
@@ -57,8 +57,33 @@ public class BasicTest extends UnitTest {
     	assertEquals("Hello world", firstPost.content);
     	assertNotNull(firstPost.postedAt);
 
-
-
-
     }
+
+    @Test
+    public void createComments(){
+    	User bob = new User("bob@example.com", "secret", "Bob").save();
+
+    	Post bobPost = new Post(bob, "My first post", "Hello world").save();
+
+    	new Comment(bobPost, "Jeff", "Nice!").save();
+    	new Comment(bobPost, "Tom", "Good!").save();
+
+    	List<Comment> bobPostComments = Comment.find("byPost", bobPost).fetch();
+
+    	assertEquals(2, bobPostComments.size());
+
+    	Comment firstComment = bobPostComments.get(0);
+    	assertNotNull(firstComment);
+    	assertEquals("Jeff", firstComment.author);
+    	assertEquals("Nice!", firstComment.content);
+    	assertNotNull(firstComment.postedAt);
+
+    	Comment secondComment = bobPostComments.get(1);
+    	assertNotNull(secondComment);
+    	assertEquals("Tom", secondComment.author);
+    	assertEquals("Good!", secondComment.content);
+    	assertNotNull(secondComment.postedAt);
+    }
+
+
 }
