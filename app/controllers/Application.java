43d86722db8165385a6db2a08b7ca801a840e7cc
yohhatu <yohhatu@gmail.com>
2012-04-28 17:28:45 +0900

    キャプチャのバリデーション(写経)

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -14,40 +14,51 @@ import models.*;
 public class Application extends Controller {
 
 	@Before
-	static void addDefaults(){
-		renderArgs.put("blogTitle", Play.configuration.getProperty("blog.title"));
-		renderArgs.put("blogBaseline", Play.configuration.getProperty("blog.blogBaseline"));
+	static void addDefaults() {
+		renderArgs.put("blogTitle",
+				Play.configuration.getProperty("blog.title"));
+		renderArgs.put("blogBaseline",
+				Play.configuration.getProperty("blog.blogBaseline"));
 	}
 
-    public static void index() {
-    	Post frontPost = Post.find("order by postedAt desc").first();
-    	List<Post> olderPosts = Post.find("order by postedAt desc").from(1).fetch(10);
-    	render(frontPost, olderPosts);
-    }
-
-    public static void show(Long id) {
-    	Post post = Post.findById(id);
-    	String randomID = Codec.UUID();
-    	render(post, randomID);
+	public static void index() {
+		Post frontPost = Post.find("order by postedAt desc").first();
+		List<Post> olderPosts = Post.find("order by postedAt desc").from(1)
+				.fetch(10);
+		render(frontPost, olderPosts);
 	}
 
-    public static void postComment(Long postId, @Required String author, @Required String content){
-    	Post post = Post.findById(postId);
-    	if (validation.hasErrors()) {
-    		render("Application/show.html", post);
+	public static void show(Long id) {
+		Post post = Post.findById(id);
+		String randomID = Codec.UUID();
+		render(post, randomID);
+	}
+
+	public static void postComment(Long postId,
+			@Required(message = "Authorは必須です") String author,
+			@Required(message = "コメントは必須です") String content,
+			@Required(message = "Please type the code") String code,
+			String randomID) {
+		Post post = Post.findById(postId);
+
+		validation.equals(code, Cache.get(randomID)).message(
+				"文字列が正しくありません。再度入力してください");
+		if (validation.hasErrors()) {
+			render("Application/show.html", post, randomID);
 		}
 
-    	post.addComment(author, content);
-    	flash.success("%s コメントありがとうございます。", author);
-    	show(postId);
-    }
+		post.addComment(author, content);
+		flash.success("%s コメントありがとうございます。", author);
+		Cache.delete(randomID);
+		show(postId);
+	}
 
-    public static void captcha(String id){
-    	Images.Captcha captcha = Images.captcha();
+	public static void captcha(String id) {
+		Images.Captcha captcha = Images.captcha();
 
-    	String code = captcha.getText("#E4EAFD");
-    	Cache.set(id, code, "10min");
-    	renderBinary(captcha);
-    }
+		String code = captcha.getText("#E4EAFD");
+		Cache.set(id, code, "10min");
+		renderBinary(captcha);
+	}
 
 }
