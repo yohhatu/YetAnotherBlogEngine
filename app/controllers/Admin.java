898f1eebf510c8f7df7f964e49c64eca116f1570
yohhatu <yohhatu@gmail.com>
2012-05-01 15:21:52 +0900

    Add NewPostFunction

--- app/controllers/Admin.java
+++ app/controllers/Admin.java
@@ -1,8 +1,10 @@
 package controllers;
 
+import java.util.Iterator;
 import java.util.List;
 
 import models.Post;
+import models.Tag;
 import models.User;
 import play.mvc.Before;
 import play.mvc.Controller;
@@ -24,5 +26,28 @@ public class Admin extends Controller{
 		render(posts);
 	}
 
+	public static void form(){
+		render();
+	}
+
+	public static void save(String title, String content, String tags) {
+	    // Create post
+	    User author = User.find("byEmail", Security.connected()).first();
+	    Post post = new Post(author, title, content);
+	    // Set tags list
+	    for(String tag : tags.split("\\s+")) {
+	        if(tag.trim().length() > 0) {
+	            post.tags.add(Tag.findOrCreateByName(tag));
+	        }
+	    }
+	    // Validate
+	    validation.valid(post);
+	    if(validation.hasErrors()) {
+	        render("@form", post);
+	    }
+	    // Save
+	    post.save();
+	    index();
+	}
 
 }
