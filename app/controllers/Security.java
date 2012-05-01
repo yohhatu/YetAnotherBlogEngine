ec53a1bdc15d914eeb5c6f071041bcaab8fb3cc4
yohhatu <yohhatu@gmail.com>
2012-05-01 12:59:02 +0900

    Add AdminRole

--- app/controllers/Security.java
+++ app/controllers/Security.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import javax.jws.soap.SOAPBinding.Use;
+
 import models.User;
 
 public class Security extends Secure.Security {
@@ -8,5 +10,14 @@ public class Security extends Secure.Security {
 		return User.connect(username, password) != null;
 		//return true;
 	}
+
+	static boolean check(String profile){
+		if("admin".equals(profile)){
+			return User.find("byEmail", connected()).<User>first().isAdmin;
+		}
+
+		return false;
+	}
+
 
 }
